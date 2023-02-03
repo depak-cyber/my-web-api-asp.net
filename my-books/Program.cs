4e21a2f6d397a05b446d4434d9eb7491b19b2c81
unknown <pantraj.deepak@gmail.com>
2023-02-03 18:55:52 +1100

    crud

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using my_books.Data;
+using my_books.Data.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+//configure the services
+builder.Services.AddTransient<BooksService>();
 
 var app = builder.Build();
 
@@ -21,5 +34,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+AppDBInitializer.Seed(app);
 
 app.Run();
